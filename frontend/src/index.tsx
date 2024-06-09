48e048ec39fc0863ee1acafb13f245d6c9ffce7d
James Mathenge Maruhi <jmaruhi@safaricom.co.ke>
2024-06-10 02:45:02 +0300

    Set up initial codebase

---
+++ frontend/src/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import client from "./apollo/Client";
+import theme from "./theme";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+root.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </ThemeProvider>,
+);
+
+reportWebVitals();
